3c7caad112f1b7723b119fc93af593207aef1dd5
Amalsha47 <bandara-ps20048@stu.kln.ac.lk>
2025-01-20 11:28:00 +0530

    change user into user info

---
+++ src/main/java/com/example/socialmedia/model/UserInfo.java
@@ -0,0 +1,29 @@
+package com.example.socialmedia.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.annotation.Id;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Document
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserInfo {
+
+    @Id
+    private String id;
+    private String username;
+    private String email;
+    private String password;
+    private String roles;
+
+    // Friends and friend request
+    private Set<String> friends = new HashSet<>(); // friends user IDs
+    private Set<String> friendsRequest = new HashSet<>(); // friends request
+
+}
