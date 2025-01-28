bc57da498857e901e3a09cf1b4e44a1f28d748d9
Amalsha47 <bandara-ps20048@stu.kln.ac.lk>
2025-01-28 22:40:44 +0530

    add chatmessage content

--- src/main/java/com/example/socialmedia/entity/ChatMessage.java
+++ src/main/java/com/example/socialmedia/entity/ChatMessage.java
@@ -1,5 +1,22 @@
 package com.example.socialmedia.entity;
 
-public class ChatMessage {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.time.LocalDateTime;
+
+@Document
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ChatMessage {
+    @Id
+    private String id;
+    private String senderId; // ID of the sender
+    private String receiverId; // ID of the receiver
+    private String message;
+    private LocalDateTime timestamp;
 }
